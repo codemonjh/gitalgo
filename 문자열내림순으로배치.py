dbbcb72027fc8d54455cc274339b7a9d83bceff7
codemonjhsource <newjh20201001@gmail.com>
2024-01-02 15:30:25 +0900

    fix

--- 문자열내림순으로배치.py
+++ 문자열내림순으로배치.py
@@ -1,14 +1,7 @@
-#https://school.programmers.co.kr/learn/courses/30/lessons/77884
+#https://school.programmers.co.kr/learn/courses/30/lessons/12917
 
-def solution(left, right):
-    answer=0
-    for i in range(left,right+1):
-        divisors=[]
-        for j in range(1,i+1):
-            if i%j==0:
-                divisors.append(j)
-        if len(divisors)%2==0:
-            answer+=i
-        else:
-            answer-=i
-    return answer
+def solution(s):
+    a=[ord(s[i:i+1]) for i in range(len(s))]
+    a.sort(reverse=True)
+    b="".join([chr(i) for i in a])
+    return b
